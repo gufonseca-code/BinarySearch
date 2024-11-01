54c391d16ac494baff0c6585e6a79f46fb2b9fcd
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2024-10-31 22:40:56 -0300

    Changed to be able to count how many times the code has run

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-fn binary_search(array: &[i32], low: i32, high: i32, x: i32) -> Option<i32> {
+static mut CC: u32 = 0;
+unsafe fn binary_search(array: &[i32], low: i32, high: i32, x: i32) -> String {
+    
     if high >= low {
         let mid = low + (high - low) / 2;
 
-        if array[mid as usize] == x { return Some(mid) }
+        if array[mid as usize] == x { return format!("Found at index {mid}, took {CC} times to finish") }
 
-        if array[mid as usize] > x { return binary_search(array, low, mid - 1, x) }
+        if array[mid as usize] > x { 
+            CC += 1;
+            return binary_search(array, low, mid - 1, x) 
+        }
 
+        CC += 1;
         return binary_search(array, low, mid + 1, x)
     }
 
-    None
+    format!("No matching index found, took {CC} times to finish")
 }
 
 fn main() {
     let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     static QUERY: i32 = 0;
-    let result = binary_search(&array, 0, array.len() as i32 - 1, QUERY);
-
-    if result.is_none() {
-        println!("No matches found");
-    } else {
-        println!("Element is present at index {:?}", result.unwrap());
-    }
-
+    unsafe { println!("{}", binary_search(&array, 0, array.len() as i32 - 1, QUERY)); }
 }
