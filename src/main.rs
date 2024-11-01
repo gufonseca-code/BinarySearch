607cd5cf4de8b6b2a5e6c50d1edf816e9ab23c21
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2024-10-31 22:48:45 -0300

    Refactored so I don't need to use unsafe blocks and functions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-static mut CC: u32 = 0;
-unsafe fn binary_search(array: &[i32], low: i32, high: i32, x: i32) -> String {
-    
+fn binary_search(array: &[i32], low: i32, high: i32, x: i32, mut cc: u32) -> String {
+   
     if high >= low {
+        
         let mid = low + (high - low) / 2;
 
-        if array[mid as usize] == x { return format!("Found at index {mid}, took {CC} times to finish") }
+        if array[mid as usize] == x { return format!("Found at index {mid}, took {cc} times to finish") }
 
         if array[mid as usize] > x { 
-            CC += 1;
-            return binary_search(array, low, mid - 1, x) 
+            cc += 1;
+            return binary_search(array, low, mid - 1, x, cc) 
+            
         }
-
-        CC += 1;
-        return binary_search(array, low, mid + 1, x)
+        
+        cc += 1;
+        return binary_search(array, low, mid + 1, x, cc)
+        
     }
 
-    format!("No matching index found, took {CC} times to finish")
+    format!("No matching index found, took {cc} times to finish")
 }
 
 fn main() {
     let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     static QUERY: i32 = 0;
-    unsafe { println!("{}", binary_search(&array, 0, array.len() as i32 - 1, QUERY)); }
+    let cc: u32 = 0;
+    println!("{}", binary_search(&array, 0, array.len() as i32 - 1, QUERY, cc));
 }
